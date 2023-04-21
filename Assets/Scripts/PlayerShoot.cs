80770483e9ba25cf95b101ab1fff20feefa1c0ff
Stéphane Prestinari <stephane.prestinari@he-arc.ch>
2023-04-21 23:25:50 +0200

    The player shoots the bullets

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] GameObject projectile;
+    [SerializeField] float timeoutBetweenBullets = 1f;
+
+    bool _alreadyShot = false;
+
+    void Update()
+    {
+        if (!_alreadyShot && (Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)))
+        {
+            Instantiate(projectile, transform.position, transform.rotation);
+            StartCoroutine(Timeout(timeoutBetweenBullets));
+        }
+    }
+
+    IEnumerator Timeout(float seconds)
+    {
+        _alreadyShot = true;
+        yield return new WaitForSeconds(seconds);
+        _alreadyShot = false;
+    }
+}
