7a5c4af161fd16f85aa9f998e658d6a57b69626a
stephchapp <stephane.chappuis@he-arc.ch>
2023-04-22 17:01:04 +0200

    Animation hurt knight + cloud death

--- Assets/Scripts/KnightController.cs
+++ Assets/Scripts/KnightController.cs
@@ -8,10 +8,15 @@ public class KnightController : MonoBehaviour
     private GameObject player;
     private GameObject camera;
     private CameraShake cameraShake;
+    private Animator knightAnimator;
+    private bool cdHurt = false;
+    [SerializeField] private GameObject cloudDeathPrefab;
 
     public Vector3 startingPosition; // the starting position of the enemy
     public float speed = 2f;
     public int lives = 3;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +25,8 @@ public class KnightController : MonoBehaviour
         camera = GameObject.Find("Main Camera");
         cameraShake = camera.GetComponent<CameraShake>();
         transform.position = startingPosition;
+
+        knightAnimator = gameObject.GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -37,17 +44,32 @@ public class KnightController : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Bullet"))
+        if (other.CompareTag("Bullet") && !cdHurt)
         {
             lives -= 1;
+            StartCoroutine(HurtAnimation());
+
             if (lives <= 0)
             {
+                // Death cloud
+                Instantiate(cloudDeathPrefab, transform.position, Quaternion.identity);
+
                 Destroy(this.gameObject);
                 //cameraShake.shakeDuration = 2;
                 //cameraShake.shakeMagnitude = 4;
                 cameraShake.Shake();
             }
         }
+    }
+
+    IEnumerator HurtAnimation()
+    {
+        knightAnimator.SetBool("isHurt", true);
+        cdHurt = true;
+        yield return new WaitForSeconds(0.2f);
+        knightAnimator.SetBool("isHurt", false);
+        cdHurt = false;
+
     }
 
 }
